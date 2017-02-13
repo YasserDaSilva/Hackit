ee214be3bb788417de1bfcce904584ca254a4858
Metourni <en_metourni@esi.dz>
2017-02-14 00:07:20 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -189,7 +189,7 @@
 	/*----------------------------------------------------*/
 	/* Final Countdown Settings
 	------------------------------------------------------ */
-	var finalDate = '2017/02/17';
+	var finalDate = '2017/02/18';
 
 	$('div#counter').countdown(finalDate)
    	.on('update.countdown', function(event) {
